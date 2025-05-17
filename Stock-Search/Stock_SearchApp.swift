2566d891ea86a606eac4882af9c1b94e56af2074
Fernando-Arenas <102317329+neo-Fernando-Arenas@users.noreply.github.com>
2025-05-17 10:58:58 -0600

    Initial commit

---
+++ Stock-Search/Stock_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stock_SearchApp.swift
+//  Stock-Search
+//
+//  Created by Fernando Arenas on 2025-05-17.
+//
+
+import SwiftUI
+
+@main
+struct Stock_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
